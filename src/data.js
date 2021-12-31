d552269a909969c1013715bbc5882cf946acb93d
Fabricio Camargo <fabriciombc@gmail.com>
2021-12-30 18:27:46 -0800

    second commit

--- src/data.js
+++ src/data.js
@@ -4,33 +4,33 @@ export const projects = [
     subtitle: "MERN Stack",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    image: "./kodosaur.jpeg",
+    link: "https://kodosaur.herokuapp.com/",
   },
   {
     title: "React Tracks",
     subtitle: "React and Python",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    image: "./dark-market.jpeg",
+    link: "https://dark-market766.herokuapp.com/",
   },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
+  // {
+  //   title: "DevChat",
+  //   subtitle: "React and Firebase",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-3.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
+  // {
+  //   title: "Epic Todo App",
+  //   subtitle: "React Hooks",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+  //   image: "./project-4.gif",
+  //   link: "https://reactbootcamp.com",
+  // },
 ];
 
 export const testimonials = [
@@ -51,10 +51,16 @@ export const testimonials = [
 ];
 
 export const skills = [
+  "Ruby on Rails",
   "JavaScript",
   "React",
-  "Next.js",
   "Node",
-  "GraphQL",
-  "Material UI",
+  "SQLite",
+  "PostGreSQL",
+  "Bootstrap",
+  "CSS Sass",
+  "Netlify",
+  "Heroku",
+  "Git",
+  "GitHub"
 ];
