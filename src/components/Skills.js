2072df76424a35bc8f941790fce39539d4569c63
Fabricio Camargo <fabriciombc@gmail.com>
2022-01-03 17:27:23 -0800

    fixed text of skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,9 +12,8 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            Here is the list of Skills and Technologies I have been acquiring and learning during the past year.
+            I am currently enrolled at FIAP University in Brazil and learning Full Stack Java Developer.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
